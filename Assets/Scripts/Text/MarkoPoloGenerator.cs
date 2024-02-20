d345e7d624d7bcf03d839726e1cfcc6e68ff8153
Your Name <you@example.com>
2024-02-20 19:43:15 +0100

    Cleanup for MarkoPolo Generator
    Fixed ScrollView, so it is possible to see all lines outside of text in the inspector. Also, now the scroll view component is invisible until you activate it via the button.

--- Assets/Scripts/Text/MarkoPoloGenerator.cs
+++ Assets/Scripts/Text/MarkoPoloGenerator.cs
@@ -9,6 +9,9 @@ public class MarkoPoloGenerator : MonoBehaviour
 {
     public TextMeshProUGUI outputText;
     public Button generateButton;
+    public ScrollRect scrollView;
+    public GameObject content;
+    public float padding = 10f;
 
     private void Start()
     {
@@ -42,5 +45,15 @@ public class MarkoPoloGenerator : MonoBehaviour
         }
 
         outputText.SetText(generatedText.ToString());
+
+        // Set the generated text as the content of the scroll view
+        outputText.text = generatedText.ToString();
+
+        // Enable scrolling by adjusting the size of the content
+        RectTransform contentRectTransform = content.GetComponent<RectTransform>();
+        contentRectTransform.sizeDelta = new Vector2(contentRectTransform.sizeDelta.x, outputText.preferredHeight + padding);
+
+        // Toggle the visibility of the scroll view
+        scrollView.gameObject.SetActive(!scrollView.gameObject.activeSelf);
     }
 }
